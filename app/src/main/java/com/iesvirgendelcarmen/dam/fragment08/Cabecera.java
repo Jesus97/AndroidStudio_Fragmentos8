46fd567364a72f7c138e961b3910ccd1460d0ee0
Jesus97 <jesusesma97@gmail.com>
2017-10-25 11:49:05 +0200

    Initial commit

---
+++ app/src/main/java/com/iesvirgendelcarmen/dam/fragment08/Cabecera.java
@@ -0,0 +1,22 @@
+package com.iesvirgendelcarmen.dam.fragment08;
+
+import android.app.ListFragment;
+import android.app.Notification;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by matinal on 25/10/2017.
+ */
+
+public class Cabecera extends ListFragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+//        Cabecera primerFragmento = new Cabecera();
+//        primerFragmento.setArguments(getIntent().getExtras());
+    }
+
+}
---
+++ app/src/main/java/com/iesvirgendelcarmen/dam/fragment08/Fragment08.java
@@ -0,0 +1,14 @@
+package com.iesvirgendelcarmen.dam.fragment08;
+
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Fragment08 extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment08);
+    }
+}
